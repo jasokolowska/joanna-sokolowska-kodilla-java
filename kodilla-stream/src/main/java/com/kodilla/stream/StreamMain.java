7bcf21f85340dc3b6d1bb3c5f08218eff89bb710
Joanna Sokolowska <joanna_s@op.pl>
2021-08-18 20:49:23 +0200

    7.2 Immutable Forum User Exercise

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExecuteSaySomething;
 import com.kodilla.stream.lambda.Executor;
 import com.kodilla.stream.lambda.ExpressionExecutor;
@@ -32,6 +33,7 @@ public class StreamMain {
         poemBeautifier.beautify("Przykladowy tekst do upiekszenia", (text) -> "***" + text + "***");
         poemBeautifier.beautify("Przykladowy tekst do upiekszenia", (text) -> text.toUpperCase());
 
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
